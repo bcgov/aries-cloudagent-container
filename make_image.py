52832b9a79f2ad3286073edf225550610ff09cb2
Emiliano Suñé <emiliano.sune@gmail.com>
2019-08-22 14:27:41 -0700

    Add 0.3.1 python release
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- make_image.py
+++ make_image.py
@@ -14,6 +14,12 @@ VERSIONS = {
             "args": {
                 "base_image": "bcgovimages/von-image:py36-1.11-0"
             },
+        },
+        {
+            "version": "0.3.1",
+            "args": {
+                "base_image": "bcgovimages/von-image:py36-1.11-0"
+            },
         }
     ],
 }

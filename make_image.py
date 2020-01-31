d0df8438a84647ca7e8358edc6df0f4f93609204
Emiliano Suñé <emiliano.sune@gmail.com>
2020-01-31 10:41:38 -0800

    Add 0.4.1 release
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- make_image.py
+++ make_image.py
@@ -50,6 +50,12 @@ VERSIONS = {
             "args": {
                 "base_image": "bcgovimages/von-image:py36-1.11-1"
             },
+        },
+        {
+            "version": "0.4.1",
+            "args": {
+                "base_image": "bcgovimages/von-image:py36-1.14-0"
+            },
         }
     ],
 }

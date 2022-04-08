fa7cf04f5cab2c3e090973792a36fefd758c99b7
Stephen Curran <swcurran@gmail.com>
2022-04-08 10:02:56 -0700

    Updates for new ACA-Py Release
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

--- make_image.py
+++ make_image.py
@@ -171,6 +171,13 @@ VERSIONS = {
                 "acapy_reqs": "[askar,bbs]"
             },
         },
+        {
+            "version": "0.7.4-rc0",
+            "args": {
+                "base_image": "bcgovimages/von-image:py36-1.16-1",
+                "acapy_reqs": "[askar,bbs]"
+            },
+        },
     ]
 }
 

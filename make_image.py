9cc87e6bab7babf1ad3de56104b0f003cd5b6eab
Stephen Curran <swcurran@gmail.com>
2022-06-16 12:53:54 -0700

    Add 0.7.4-rc3
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

--- make_image.py
+++ make_image.py
@@ -192,6 +192,13 @@ VERSIONS = {
                 "acapy_reqs": "[askar,bbs]"
             },
         },
+        {
+            "version": "0.7.4-rc3",
+            "args": {
+                "base_image": "bcgovimages/von-image:py36-1.16-1",
+                "acapy_reqs": "[askar,bbs]"
+            },
+        },
     ]
 }
 

6fe8e80f1c8ead8b15e04a709f6346887c72129a
Stephen Curran <swcurran@gmail.com>
2023-07-26 17:31:51 +0000

    0.9.0
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

--- make_image.py
+++ make_image.py
@@ -193,10 +193,10 @@ VERSIONS = {
             },
         },
         {
-            "version": "0.9.0-rc0",
+            "version": "0.9.0",
             "dockerfile": "Dockerfile.mirror",
             "args": {
-                "base_image": "ghcr.io/hyperledger/aries-cloudagent-python:py3.9-indy-1.16.0-0.9.0-rc0"
+                "base_image": "ghcr.io/hyperledger/aries-cloudagent-python:py3.9-indy-1.16.0-0.9.0"
             },
         },
         {
